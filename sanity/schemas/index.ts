d3249ad0c3a10027de78e26ea56e189cc225e620
aditya <adityasingh770@protonmail.com>
2023-12-23 18:45:40 +0530

    feat: resource-playlist completed and exported

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,6 @@
+import resource from './resource.schema';
+import resourcePlaylist from './resoure-playlist.schema';
+
+const schemas = [resource, resourcePlaylist];
+
+export default schemas;
--- sanity/schemas/resource.schema.ts
+++ sanity/schemas/resource.schema.ts
@@ -50,3 +50,5 @@ const schema = {
     },
   ],
 };
+
+export default schema;
