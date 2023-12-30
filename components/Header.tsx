43e69206220916b8a70f6c7962fd30d6c827e112
aditya <adityasingh770@protonmail.com>
2023-12-30 20:53:33 +0530

    feat: headers made dynamic feat: search functionality auto hidden

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+interface Props {
+  title: string;
+  query?: string;
+  category?: string;
+}
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ title, query, category }: Props) => {
+  if (query && category) {
+    return (
+      <h1 className="heading3 self-start text-white-800">
+        Search results for &quot;{query}&quot; in {category}
+      </h1>
+    );
+  } else if (query) {
+    return <h1 className="heading3 self-start text-white-800">Search results for &quot;{query}&quot;</h1>;
+  } else if (category) {
+    return <h1 className="heading3 self-start text-white-800">{category}</h1>;
+  }
+  return <h1 className="heading3 self-start text-white-800">No results</h1>;
 };
 
 export default Header;
