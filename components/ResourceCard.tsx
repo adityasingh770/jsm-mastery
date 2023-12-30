4d4894d0393fa9c233a4a568d975fa8dd9e1d3ad
aditya <adityasingh770@protonmail.com>
2023-12-30 13:55:24 +0530

    feat: resource card implemented

---
+++ components/ResourceCard.tsx
@@ -0,0 +1,33 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface Props {
+  id: string;
+  title: string;
+  image: string;
+  downloadNumber: number;
+  slug: string;
+}
+
+const ResourceCard = ({ id, title, image, downloadNumber, slug }: Props) => {
+  return (
+    <Card className="w-full max-w-fit border-0 !bg-transparent sm:max-w-[356px]">
+      <Link href={`/resource/${id}`}>
+        <CardHeader>
+          <Image src={image} className="h-full rounded-md object-cover" width={384} height={440} alt={title} />
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+      </Link>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default ResourceCard;
